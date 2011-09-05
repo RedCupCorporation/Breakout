bb8f3d81ec5a37ee6afd576b2a10304e22f0494e
S <s>
2011-09-05 11:55:59 -0700

    {"type":"SWC", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -60,7 +60,13 @@ public class Breakout extends GraphicsProgram {
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
-		/* You fill this in, along with any subsidiary methods */
+		setup();
+		play();
+	}
+	
+	private void setup() {
+		
+		
 	}
 
 }
