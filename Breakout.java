0ec7afbd1d28187837d1c91274078f39fa969c16
S <s>
2011-09-05 13:57:06 -0700

    {"type":"SWC", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -89,18 +89,18 @@ public class Breakout extends GraphicsProgram {
 	private void drawRow(Color color, int y) {
 		for (int i = 0; i < NBRICKS_PER_ROW; i++) {
 			int widthOfAllBricks = BRICK_WIDTH * NBRICKS_PER_ROW + BRICK_SEP * (NBRICKS_PER_ROW - 1);
-			double firstRowX = (WIDTH - widthOfAllBricks) / 2;
-			double x = firstRowX + i * (BRICK_WIDTH + BRICK_SEP);
+			int firstRowX = (WIDTH - widthOfAllBricks) / 2;
+			int x = firstRowX + i * (BRICK_WIDTH + BRICK_SEP);
 			drawBrick(x, y, color);
 		}
 	}
 	
-	private void drawBrick(double x, int y, Color color) {
+	private void drawBrick(int x, int y, Color color) {
 		add(new GBrick(BRICK_WIDTH, BRICK_HEIGHT, color), x, y);
 	}
 	
 	private void drawPaddle() {
-		
+		add(new GBrick(PADDLE_WIDTH, PADDLE_HEIGHT, Color.black), (WIDTH - PADDLE_WIDTH) / 2, HEIGHT - PADDLE_Y_OFFSET);
 	}
 	
 	private void drawBall() {
