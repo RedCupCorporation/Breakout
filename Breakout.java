48f72590b3ca05f2ab53a6fda094da5ade964e64
S <s>
2011-09-05 20:03:53 -0700

    {"type":"SWC", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -145,21 +145,22 @@ public class Breakout extends GraphicsProgram {
 		/*
 		 * Optimization: only check 3 corners in the direction the ball is heading
 		 */
+		GObject collidee = null;
 		GPoint left = new GPoint(ball.getX(), ball.getY() + BALL_RADIUS);
 		GPoint right = new GPoint(ball.getX() + 2 * BALL_RADIUS, ball.getY() + BALL_RADIUS);
 		GPoint top = new GPoint(ball.getX() + BALL_RADIUS, ball.getY());
 		GPoint bottom = new GPoint(ball.getX() + BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
 		if (getElementAt(bottom) != null) {
-			GObject collidee = getElementAt(bottom);
+			collidee = getElementAt(bottom);
 			surface = 6;
 		} else if (getElementAt(top) != null) {
-			GObject collidee = getElementAt(top);
+			collidee = getElementAt(top);
 			surface = 12;
 		} else if (getElementAt(left) != null) {
-			GObject collidee = getElementAt(left);
+			collidee = getElementAt(left);
 			surface = 9;
 		} else if (getElementAt(right) != null) {
-			GObject collidee = getElementAt(right);
+			collidee = getElementAt(right);
 			surface = 3;
 		}
 		return collidee;
