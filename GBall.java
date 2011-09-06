0fbb92c3f73944f86e6f4f4c6690fb07c53f8410
S <s>
2011-09-06 01:28:39 -0700

    {"type":"SWC", "runs":"0"}

--- GBall.java
+++ GBall.java
@@ -1,4 +1,18 @@
+/**
+ * Creates a filled brick to be used in Breakout
+ * @author kenziemac244
+ *
+ */
 
-public class GBall {
+import java.awt.*;
+import acm.graphics.*;
+
+public class GBall extends GOval {
+	
+	public GBall(int width, int height, Color color) {
+		super(width, height);
+		setFilled(true);
+		setFillColor(color);
+	}
 
 }
