2707e2a658f70cf5c65ae0532eb4610d0a632988
S <s>
2011-09-26 12:20:35 -0700

    {"type":"SWC", "runs":"0"}

--- GCollidee.java
+++ GCollidee.java
@@ -1,4 +1,16 @@
+/**
+ * Creates a filled brick to be used in Breakout
+ * @author kenziemac244
+ *
+ */
 
-public class GCollidee {
+import java.awt.*;
+import acm.graphics.*;
+
+public class GCollidee extends GObject {
+	
+	public GCollidee(GObject object) {
+		super(object);
+	}
 
 }
